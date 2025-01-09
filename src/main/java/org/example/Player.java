712b98e40d61b94f7bb95d153b9b3d0ba71d088d
Büşra Yılmaz <124158606+Busraylmzz1@users.noreply.github.com>
2025-01-10 00:46:04 +0300

    s13d4 refresh

--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -3,5 +3,42 @@ package org.example;
 public class Player {
     String name;
     int healthPercentage;
-    Weapon weapon;
+    private Weapon weapon;
+    public Player(String name, int healthPercentage,Weapon weapon){
+        this.name=name;
+        this.healthPercentage=healthPercentage;
+        checkHealth(healthPercentage);
+        this.weapon=weapon;
+    }
+    private void checkHealth(int healthPercentage){
+        if(healthPercentage< 0){
+            this.healthPercentage=0;
+        } if (healthPercentage>100){
+        this.healthPercentage=100;
+
+            }
+
+        }
+        public int getHealthRemaining(){
+        return this.healthPercentage;
+        }
+        public void loseHealth(int damage){
+        int healthRemain = healthPercentage-damage;
+        if(healthRemain<= 0){
+            System.out.println(name + " player has been knocked out of game");
+
+        }
+        healthPercentage= healthRemain;
+        checkHealth(healthPercentage);
+        }
+        public void restoreHealth(int healthPotion){
+        this.healthPercentage+= healthPotion;
+        checkHealth(this.healthPercentage);
+        }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
+
--- src/main/java/org/example/Weapon.java
+++ src/main/java/org/example/Weapon.java
@@ -1,7 +1,20 @@
 package org.example;
 
 public enum Weapon {
-    SWORD;
-    int damage;
-    double attackSpeed;
+    SWORD(20,4),
+    AXE(20,5),
+    MACE(40,7);
+private int damage;
+private double attackSpeed;
+
+Weapon(int damage, double attackSpeed){
+this.attackSpeed=attackSpeed;
+this.damage=damage;
+}
+public int getDamage(){
+    return this.damage;
+}
+public double getAttackSpeed(){
+    return this.attackSpeed;
+}
 }
